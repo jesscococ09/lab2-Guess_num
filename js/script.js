c106f5985bd80a833283a95be6b365357d8fb36e
jesscococ09 <jesstorrealba@gmail.com>
2026-02-14 19:46:26 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// event listeners
+document.querySelector("#guessBtn").addEventListener("click", guess);
+document.querySelector("#playBtn").addEventListener("click", reset);
+
+// global variables
+let random = Math.floor(Math.random() * 99) + 1; // 1-99
+let attempts = 0;
+let wins = 0;
+let losses = 0;
+
+console.log(random);
+
+let hiLo = document.querySelector("#hiLo");
+
+document.querySelector("#playBtn").style.display = "none";
+
+// functions
+function isValid(userGuess) {
+    if (!Number.isInteger(userGuess)) {
+        alert("Error: guess is not a number");
+        return false;
+    }
+
+    if (userGuess < 1 || userGuess > 100) {
+        alert("Error: guess out of range, guess num 1 to 100");
+        return false;
+    }
+
+    return true;
+}
+
+function guess() {
+    // .value is only for input elements
+    let userGuess = parseInt(document.querySelector("#userGuess").value);
+    // alert(userGuess);
+
+    if (!isValid(userGuess)) {
+        return;
+    }
+
+    document.querySelector("#userGuesses").textContent += `${userGuess} `;
+    document.querySelector("#userGuesses").style.color = "red";
+
+    attempts++;
+
+    if (userGuess > random) {
+        hiLo.textContent = "Too high!"
+        hiLo.style.color = "red";
+    } else if (userGuess < random) {
+        hiLo.textContent = "Too low!"
+        hiLo.style.color = "blue";
+    } else {
+        hiLo.textContent = "Congratulations!"
+        hiLo.style.color = "green";
+        document.querySelector("#userGuesses").textContent = "";
+        wins++;
+        document.querySelector("#playBtn").style.display = "block";
+        document.querySelector("#guessBtn").style.display = "none";
+    }
+
+    if (attempts == 7 && userGuess !==random) {
+        hiLo.textContent = `You lost! The number was ${random}.`;
+        hiLo.style.color = "red";
+        document.querySelector("#userGuesses").textContent = "";
+        losses++;
+        document.querySelector("#playBtn").style.display = "inline";
+        document.querySelector("#guessBtn").style.display = "none";
+    }
+
+    document.querySelector("#attempts").textContent = attempts;
+    document.querySelector("#wins").textContent = wins;
+    document.querySelector("#losses").textContent = losses;
+}
+
+function reset() {
+    attempts = 0;
+    random = Math.floor(Math.random() * 99) + 1;
+    document.querySelector("#playBtn").style.display = "none";
+    document.querySelector("#guessBtn").style.display = "inline";
+
+    hiLo.textContent = "";
+    document.querySelector("#attempts").textContent = attempts;
+
+    console.log(random);
+}
